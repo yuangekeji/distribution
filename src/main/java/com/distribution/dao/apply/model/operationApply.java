9df0141df5101be8213c52cb5f00794630129b2f
Bright <dongzhiming945@126.com>
2017-09-02 19:56:21 +0800

    生成申请成为运营中心mapper

---
+++ src/main/java/com/distribution/dao/apply/model/operationApply.java
@@ -0,0 +1,86 @@
+package com.distribution.dao.apply.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class operationApply {
+    private Integer id;
+
+    private Integer memberId;
+
+    private BigDecimal totalOrderAmount;
+
+    private String status;
+
+    private Integer createId;
+
+    private Date createTime;
+
+    private Integer updateId;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Integer memberId) {
+        this.memberId = memberId;
+    }
+
+    public BigDecimal getTotalOrderAmount() {
+        return totalOrderAmount;
+    }
+
+    public void setTotalOrderAmount(BigDecimal totalOrderAmount) {
+        this.totalOrderAmount = totalOrderAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public Integer getCreateId() {
+        return createId;
+    }
+
+    public void setCreateId(Integer createId) {
+        this.createId = createId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getUpdateId() {
+        return updateId;
+    }
+
+    public void setUpdateId(Integer updateId) {
+        this.updateId = updateId;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}

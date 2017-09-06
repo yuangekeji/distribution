e2a46f9a12a0627a03d79c589c47dcdc5175e278
DongShiqing <18640947990@163.com>
2017-09-06 23:45:30 +0800

    全国董事奖金日job

--- src/main/java/com/distribution/job/ChinaPresidentBonusJob.java
+++ src/main/java/com/distribution/job/ChinaPresidentBonusJob.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 
 /**
  * Created by shiqing on 2017/9/5.
@@ -14,9 +16,10 @@ public class ChinaPresidentBonusJob {
 
     @Autowired
     private ChinaPresidentBonusService chinaPresidentBonusService;
-    //@Scheduled(cron = "0/10 * * * * *")  //测试用
+    //@Scheduled(cron = "0/30 * * * * *")  //测试用
     @Scheduled(cron ="0 0 1 * * ?" )//每天早上1点钟执行
     public void addChinaPresidentBonusByDay() {
-        chinaPresidentBonusService.addChinaPresidentBonusByDay();
+        Map map = chinaPresidentBonusService.addChinaPresidentBonusByDay();
+        chinaPresidentBonusService.jobLogs(map);
     }
 }

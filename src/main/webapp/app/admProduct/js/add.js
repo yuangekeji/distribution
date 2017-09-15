77289ce31879d4eb4d676c960b7f4b2d5de4d56c
Bright <dongzhiming945@126.com>
2017-09-15 10:48:25 +0800

    添加商品图片上传功能

--- src/main/webapp/app/admProduct/js/add.js
+++ src/main/webapp/app/admProduct/js/add.js
@@ -1,6 +1,8 @@
 angular.module('admProduct').controller('admProductAddCtrl',function ($q, title, $scope, $http,  $state, Notify, $sessionStorage) {
     title.setTitle('添加商品');
-    $scope.goods = {};
+    $scope.goods = {
+        imgeUrl:""
+    };
     $scope.submitFlag = true;
     $scope.goodsTypes = [];
 
@@ -77,5 +79,56 @@ angular.module('admProduct').controller('admProductAddCtrl',function ($q, title,
     $scope.stopLoading=function () {
         App.unblockUI(e1);
     };
-});
 
+    /**图片上传start*/
+    $scope.reader = new FileReader();   //创建一个FileReader接口
+    $scope.form = {     //用于绑定提交内容，图片或其他数据
+        image:{},
+    };
+    $scope.thumb = {};      //用于存放图片的base64
+    $scope.thumb_default = {    //用于循环默认的‘加号’添加图片的框
+        1111:{}
+    };
+
+    $scope.img_upload = function(files) {       //单次提交图片的函数
+        $scope.guid = (new Date()).valueOf();   //通过时间戳创建一个随机数，作为键名使用
+        $scope.reader.readAsDataURL(files[0]);  //FileReader的方法，把图片转成base64
+        $scope.reader.onload = function(ev) {
+            $scope.$apply(function(){
+                $scope.thumb[$scope.guid] = {
+                    imgSrc : ev.target.result,  //接收base64
+                }
+            });
+        };
+
+        var data = new FormData();      //以下为像后台提交图片数据
+        data.append('image', files[0]);
+        data.append('guid',$scope.guid);
+        $http({
+            method: 'post',
+            url: ctx + '/admGoods/uploadImage',
+            data:data,
+            headers: {'Content-Type': undefined},
+            transformRequest: angular.identity
+        }).success(function(resp) {
+            if (resp.successful) {
+                $scope.goods.imgeUrl = resp.data;
+                console.log($scope.form)
+            }else{
+                Notify.warning(resp.data);
+            }
+        }).error(function (resp) {
+            console.log(resp);
+        })
+    };
+
+    $scope.img_del = function(key) {    //删除，删除的时候thumb和form里面的图片数据都要删除，避免提交不必要的
+        var guidArr = [];
+        for(var p in $scope.thumb){
+            guidArr.push(p);
+        }
+        delete $scope.thumb[guidArr[key]];
+        delete $scope.form.image[guidArr[key]];
+    };
+    /**图片上传end*/
+});

3a57e5d3cb7a565c6f964e3255ee32a25fd0355b
Bright <dongzhiming945@126.com>
2017-10-15 21:47:09 +0800

    创建公告时添加操作记录

--- src/main/java/com/distribution/service/AdmNoticeService.java
+++ src/main/java/com/distribution/service/AdmNoticeService.java
@@ -1,5 +1,6 @@
 package com.distribution.service;
 
+import com.distribution.common.constant.Constant;
 import com.distribution.common.utils.Page;
 import com.distribution.dao.admin.model.Admin;
 import com.distribution.dao.notice.mapper.NoticeMapper;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class AdmNoticeService {
@@ -17,6 +20,8 @@ public class AdmNoticeService {
     private MoreNoticeMapper moreNoticeMapper;
     @Autowired
     private NoticeMapper noticeMapper;
+    @Autowired
+    private AdmHandleHistoryService admHandleHistoryService;
 
     /**
      * description 公告管理列表查询
@@ -33,8 +38,10 @@ public class AdmNoticeService {
      * @author Bright
      * */
     public Integer insertOrUpdate(Notice notice,Admin admin){
+        Integer it;
+        Integer id;
         if(null!=notice.getId()){
-            return noticeMapper.updateByPrimaryKeySelective(notice);
+            it = noticeMapper.updateByPrimaryKeySelective(notice);
         }else{
             notice.setPublishingState("N");
             notice.setDeteleFlag("N");
@@ -42,8 +49,19 @@ public class AdmNoticeService {
             notice.setCreateTime(new Date());
             notice.setUpdateBy(admin.getId().toString());
             notice.setUpdateTime(new Date());
-            return noticeMapper.insert(notice);
+            it = moreNoticeMapper.insert(notice);
         }
+
+        id = notice.getId();
+        //管理员操作记录
+        Map mapHandle = new HashMap();
+        String orderNoHandle = id+"";
+        mapHandle.put("handleType", Constant.ADMINHANDLETYPE_ADDNOTICE);
+        mapHandle.put("handleId", orderNoHandle);
+        mapHandle.put("handleComment", "订单号: " + orderNoHandle + ", 操作: 订单下载");
+        admHandleHistoryService.addAdminHandleHistory(admin, mapHandle);
+
+        return it;
     }
 
     /**
